a8481ea74c62832ca73d831eaeff22c02ecb8ac1
PriyangaSrinivasan <priyalaksha17@gmail.com>
2024-06-10 17:01:19 +0530

    completed

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Nodejs-File System
+
+File system: To handle file operations like creating, reading and deleting
+Node.js provides an inbuilt module called FS (File System).
+
+Packages: `npm i express date-fns path`
+
+Reference:https://documenter.getpostman.com/view/35140244/2sA3XLFQDe
---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import fs from 'fs';
+import { format} from 'date-fns';
+import path  from 'path';
+
+//declaration/initailization
+const app = express();
+const PORT = 4000;
+
+//Middleware
+app.use(express.json())
+
+//routes
+
+//Create a text file
+app.get('/create',(req,res)=>{
+   
+    let today = format(new Date(),'dd-mm-yyyy-HH-mm-ss')
+    const filepath =`Timestamps/${today}.txt`;
+    fs.writeFileSync(filepath,`${today}`,'utf8')
+    res.status(200).send("File Created Successfully")
+})
+
+//Read all the text file
+app.get('/read',(req,res)=>{
+    let timestampsfolder = '/Timestamps'
+    let folder = path.basename(timestampsfolder)
+        let data = fs.readdirSync(folder);
+        res.status(200).send(data)
+
+})
+
+//running port
+app.listen(PORT,()=>{
+    console.log(`App is listening on the port ${PORT}`)
+})
