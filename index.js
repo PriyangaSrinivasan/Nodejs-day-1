c5a5b911a939f8afce9f76da673368d8e0064419
PriyangaSrinivasan <priyalaksha17@gmail.com>
2024-06-10 17:18:32 +0530

    updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 //routes
 app.get('/',(req,res)=>{
-    res.status(200).send(`h1 style="background-color:lightpink;padding:10px 0px;text-align:center">Express server is connected Successfully</h1>`)
+    res.status(200).send(`<h1 style="background-color:lightpink;padding:10px 0px;text-align:center">Express server is connected Successfully</h1>`)
 })
 //Create a text file
 app.get('/create',(req,res)=>{
